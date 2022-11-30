7a62b50c2defbe8a610df80fc6c3651e5b684228
Emmy-Akintz <115672480+Emmy-Akintz@users.noreply.github.com>
2022-11-30 10:55:19 +0100

    Create

---
+++ src/pages/Account.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+function Account() {
+    return (
+        <div className="">
+            <Navbar />
+            <h1>Your Account</h1>
+        </div>
+    )
+}
+
+export default Account
---
+++ src/pages/Logout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+function Logout() {
+    return (
+        <div className="">
+            <Navbar />
+            <h1>Logout</h1>
+        </div>
+    )
+}
+
+export default Logout
